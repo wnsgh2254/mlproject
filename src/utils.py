16d4b8bef86e2fe9f376967c0714e8241c4651f0
Junho Sim <https://github.com/wnsgh2254>
2023-07-17 15:17:04 +1000

    Hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 from src.exception import CustomException
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 import numpy as np
 import pandas as pd
 import dill
@@ -19,7 +19,7 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_model(X_train,y_train,X_test,y_test,models,param):
     try:
         # X_train, X_test, y_train, y_test = train_test_split(
         #     X,y,test_size = 0.2, random_state = 42
@@ -29,8 +29,13 @@ def evaluate_model(X_train,y_train,X_test,y_test,models):
  
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
+            
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
-            model.fit(X_train, y_train) # Train model
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
 
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
